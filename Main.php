58b16244f0b2b739eadc1f7fefda7624fee13d4d
LukasDev067 <aldemirpalmeiras243@gmail.com>
2024-12-06 10:37:41 -0300

    Update Main.php

--- Main.php
+++ Main.php
@@ -38,6 +38,22 @@ class Main extends PluginBase implements Listener {
      $sender->sendMessage("§euse /systemtag comprar <tag> \n \n §e/systemtag lista");
       return false;
      }
+     if(strtolower($args[0]) == "comprar"){
+       if(!isset($args[1])){
+      $sender->sendMessage("§e/systemtag comprar <tag>");
+       return;
+     }
+     if(strtolower($args[1]) == "Killer"){
+      $count = $this->contador[$sender->getName()];
+       if($count < 10){
+        $sender->sendMessage("§cPontos insuficiência");
+         return;
+       }
+       if($count >= 10){
+        
+       }
+      }
+     }
     }
    }
   }
